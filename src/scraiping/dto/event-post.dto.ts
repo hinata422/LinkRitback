014d8bd73036e518f0cae6d9c061228d27a650f3
hasseeee <hasegawa.takuya1204@gmail.com>
2025-12-18 18:20:03 +0900

    fix: add class-validator and fix dto formatting

--- src/scraiping/dto/event-post.dto.ts
+++ src/scraiping/dto/event-post.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, IsNotEmpty, IsDate, IsOptional, IsUUID } from 'class-validator';
+import {
+  IsString,
+  IsNotEmpty,
+  IsDate,
+  IsOptional,
+  IsUUID,
+} from 'class-validator';
 
 export class CreateEventPostDto {
   @IsUUID()
