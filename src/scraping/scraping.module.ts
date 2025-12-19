de88c253739afe4998727595dac983c06045c461
hasseeee <hasegawa.takuya1204@gmail.com>
2025-12-20 05:56:36 +0900

    feat: register scraping controller

--- src/scraping/scraping.module.ts
+++ src/scraping/scraping.module.ts
@@ -3,9 +3,11 @@ import { ScrapingService } from './scraping.service';
 import { RitsumeikanStrategy } from './strategies/ritsumeikan.strategy';
 import { ScraperFactory } from './scraper.factory';
 import { EventsModule } from '../events/events.module';
+import { ScrapingController } from './scraping.controller';
 
 @Module({
   imports: [EventsModule],
+  controllers: [ScrapingController],
   providers: [ScrapingService, ScraperFactory, RitsumeikanStrategy],
   exports: [ScrapingService],
 })
